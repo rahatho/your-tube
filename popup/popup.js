ba5c6280593c8a3acb697bb2108e15f8ed6bf236
Rahat Ho <rahat.ho@gmail.com>
2023-11-06 16:08:49 +0600

    init

---
+++ popup/popup.js
@@ -0,0 +1,8 @@
+console.log('Hello from the popup');
+
+document.querySelector("#btn").addEventListener('click',() => {
+    chrome.runtime.sendMessage({text: "Popup"});});
+
+chrome.runtime.onMessage.addListener((msg) => {
+    document.body.innerHTML += '<div>${msg.text}</div>';
+});
